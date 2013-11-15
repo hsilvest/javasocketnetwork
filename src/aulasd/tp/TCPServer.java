c1b77bfc3d18f39becb14654c0df4053d53c6e8e
Henrique <hs.ccti@gmail.com>
2013-11-15 02:22:17 -0200

    implementando multiusuarios

--- src/aulasd/tp/TCPServer.java
+++ src/aulasd/tp/TCPServer.java
@@ -1,34 +1,62 @@
 package aulasd.tp;
 
-//Aguarda pedido de conexão de algum cliente
-//Lê uma linha do cliente
-//Transforma as minúsculas em maiúsculas
-//Envia ao cliente
-//Volta para o início
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
-public class TCPServer {
+public class TCPServer extends Thread {
+
+	private Socket conexao;
+	private static ArrayList<DataOutputStream> clientes = new ArrayList<>();
+
+	public TCPServer(Socket socket) {
+		this.conexao = socket;
+	}
 
 	public static void main(String[] args) throws Exception {
-		String clientSentence;
-		String capitalizedSentence;
-		// cria socket de comunicação com os clientes na porta 6789
-		ServerSocket welcomeSocket = new ServerSocket(6788);
-		// espera msg de algum cliente e trata
-		while (true) {
-			// espera conexão de algum cliente
-			Socket connectionSocket = welcomeSocket.accept();
-			// cria streams de entrada e saida com o cliente que chegou
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-			// lê uma linha do cliente
-			clientSentence = inFromClient.readLine();
-			// transforma a linha em maiúsculas
-			capitalizedSentence = clientSentence.toUpperCase() + '\n';
-			// envia a linha maiúscula para o cliente
-			outToClient.writeBytes(capitalizedSentence);
-
+		try {
+			ServerSocket welcomeSocket = new ServerSocket(6787);
+			System.out.println("Servidor rodando na porta 6788");
+			while (true) {
+				Socket connectionSocket = welcomeSocket.accept();
+				Thread thread = new TCPServer(connectionSocket);
+				thread.start();
+			}
+		} catch (IOException ex) {
+			System.out.println("Erro: " + ex);
 		}
 	}
+
+	@Override
+	public void run() {
+		try {
+			BufferedReader inFromClient = new BufferedReader(
+					new InputStreamReader(this.conexao.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(
+					this.conexao.getOutputStream());
+			clientes.add(outToClient);
+			String mensagem = inFromClient.readLine();
+			while (!mensagem.toUpperCase().equals("exit")) {
+				broadCast(outToClient, "escreveu", mensagem);
+				mensagem = inFromClient.readLine();
+			}
+			clientes.remove(outToClient);
+			this.conexao.close();
+		} catch (IOException ex) {
+			System.out.println("Erro: " + ex);
+		}
+	}
+
+	public void broadCast(DataOutputStream out, String action, String msg) throws IOException {
+		Enumeration elementos = (Enumeration) clientes;
+		while (elementos.hasMoreElements()) {
+			DataOutputStream envia = (DataOutputStream) elementos.nextElement();
+			if(envia != out){
+				envia.writeBytes(msg);
+			}
+			
+		}
+	}
+
 }
--- src/aulasd/tp/TPClient.java
+++ src/aulasd/tp/TPClient.java
@@ -1,42 +1,59 @@
 package aulasd.tp;
 
-// Abre conexão com o servidor
-// Lê uma linha do teclado
-// Envia a linha ao servidor
-// Lê uma linha do servidor e mostra no vídeo
-// Fecha conexão com o servidor
-import java.io.*; // classes para input e output streams
-import java.net.*; // classes para socket, serversocket e clientsocket
+import java.io.*;
+import java.net.*;
 
-public class TPClient {
+public class TPClient extends Thread {
+
+	private String sentence;
+	private String modifiedSentence;
+	private Socket conexao;
+
+	public TPClient(Socket clientSocket) {
+		this.conexao = clientSocket;
+	}
 
 	public static void main(String[] args) throws Exception {
-		String sentence;
-		String modifiedSentence;
-		while (true) {
-			// cria o stream do teclado
-			BufferedReader inFromUser = new BufferedReader(
-					new InputStreamReader(System.in));
-			// cria o socket de acesso ao server hostname na porta 6789
-			Socket clientSocket = new Socket("127.0.0.1", 6788);
-			// cria os streams (encadeamentos) de entrada e saida com o servidor
-			DataOutputStream outToServer = new DataOutputStream(
-					clientSocket.getOutputStream());
-			BufferedReader inFromServer = new BufferedReader(
-					new InputStreamReader(clientSocket.getInputStream()));
-			// le uma linha do teclado e coloca em sentence
-			sentence = inFromUser.readLine();
-			// envia a linha para o server
-			outToServer.writeBytes(sentence + '\n');
-			// lê uma linha do server
-			modifiedSentence = inFromServer.readLine();
-			// apresenta a linha do server no vídeo
-			System.out.println("FROM SERVER " + modifiedSentence);
-			if (sentence.toUpperCase().equals("EXIT")) {
-				// fecha o cliente
-				clientSocket.close();
-				break;
+		try {
+			while (true) {
+				Socket clientSocket = new Socket("127.0.0.1", 6787);
+
+				DataOutputStream outToServer = new DataOutputStream(
+						clientSocket.getOutputStream());
+				BufferedReader inFromUser = new BufferedReader(
+						new InputStreamReader(System.in));
+				Thread thread = new TPClient(clientSocket);
+				thread.start();
+				String msg;
+				while (true) {
+					System.out.println("Mensagem: ");
+					msg = inFromUser.readLine();
+					outToServer.writeBytes(msg);
+				}
 			}
+		} catch (IOException ex) {
+			System.out.println("Erro: " + ex);
+		}
+
+	}
+
+	@Override
+	public void run() {
+		try {
+			BufferedReader inFromServer = new BufferedReader(
+					new InputStreamReader(this.conexao.getInputStream()));
+			String msg;
+			while (true) {
+				msg = inFromServer.readLine();
+				if(msg.toUpperCase().equals("EXIT")){
+					this.conexao.close();
+				}
+				System.out.println();
+				System.out.println(msg);
+				System.out.println("Mensagem: ");
+			}
+		} catch (IOException ex) {
+			System.out.println("Erro: " + ex);
 		}
 
 	}
